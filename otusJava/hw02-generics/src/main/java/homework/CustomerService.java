9334c81e5cbb6549d09c97ea8391b402fa41152a
Irina.TSygarova <Irina.Kononova@bercut.com>
2024-05-17 17:04:53 +0300

    test completed hw02

---
+++ otusJava/hw02-generics/src/main/java/homework/CustomerService.java
@@ -0,0 +1,37 @@
+package homework;
+
+import java.util.AbstractMap;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class CustomerService {
+
+    // todo: 3. надо реализовать методы этого класса
+    // важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
+    private final TreeMap<Customer, String> customers = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
+
+    public Map.Entry<Customer, String> getSmallest() {
+        return clone(customers.firstEntry());
+    }
+
+    public Map.Entry<Customer, String> getNext(Customer customer) {
+        return clone(customers.higherEntry(customer));
+    }
+
+    public void add(Customer customer, String data) {
+        customers.put(customer, data);
+    }
+
+    private Map.Entry<Customer, String> clone(Map.Entry<Customer, String> entry) {
+        if (entry != null) {
+            if (entry.getKey() == null) {
+                return new AbstractMap.SimpleImmutableEntry<>(null, entry.getValue());
+            } else {
+                return new AbstractMap.SimpleImmutableEntry<>(entry.getKey().clone(), entry.getValue());
+            }
+        } else {
+            return entry;
+        }
+    }
+}
--- otusJava/settings.gradle.kts
+++ otusJava/settings.gradle.kts
@@ -1,5 +1,6 @@
 rootProject.name = "otusJava"
 include("hw01-gradle")
+include("hw02-generics")
 
 
 pluginManagement {
@@ -23,3 +24,4 @@ pluginManagement {
         id("com.diffplug.spotless") version spotless
     }
 }
+include("hw02-generics")
