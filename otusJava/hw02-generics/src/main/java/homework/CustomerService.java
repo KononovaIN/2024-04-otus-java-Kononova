11bbecb98cef70b8cc76e2870101328e155861c3
Irina.TSygarova <Irina.Kononova@bercut.com>
2024-05-17 17:06:08 +0300

    удалены todo

--- otusJava/hw02-generics/src/main/java/homework/CustomerService.java
+++ otusJava/hw02-generics/src/main/java/homework/CustomerService.java
@@ -6,9 +6,6 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class CustomerService {
-
-    // todo: 3. надо реализовать методы этого класса
-    // важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
     private final TreeMap<Customer, String> customers = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
 
     public Map.Entry<Customer, String> getSmallest() {
